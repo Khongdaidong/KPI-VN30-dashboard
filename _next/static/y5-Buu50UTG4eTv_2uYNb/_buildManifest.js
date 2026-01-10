2d32dfb0fd6401d3f27d83adb5820d2aaf389b69
Không dài dòng <nguyenvinh.ftu2@gmail.com>
2026-01-10 10:14:01 +0800

    Updates

---
+++ _next/static/y5-Buu50UTG4eTv_2uYNb/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/KPI-VN30-dashboard//_next/:path+",
+        "destination": "/KPI-VN30-dashboard/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
